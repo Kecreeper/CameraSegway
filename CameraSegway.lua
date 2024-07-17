1a9d11c3b485f26f048d0502b7e18bf0ab3e9a86
Kecreeper <Kecreeper@gmail.com>
2024-07-17 02:44:03 -0400

    1

--- CameraSegway.lua
+++ CameraSegway.lua
@@ -1,3 +1,32 @@
 local CameraSegway = {}
 
+local CameraSegway = {}
+CameraSegway.__index = CameraSegway
+
+function CameraSegway.new(camType: string)
+    local self = setmetatable({}, CameraSegway)
+    if camType == "Static" then
+        self.camType = "Static"
+    elseif camType == "Dynamic" then
+        self.camType = "Dynamic"
+        self.start
+        self.goal
+    end
+
+    return self
+end 
+
+function CameraSegway.setup(start: BasePart | Cframe, goal: BasePart | CFrame)
+    if start:IsA("BasePart") then
+        self.start = start.CFrame 
+    elseif start:IsA("CFrame") then
+        self.start = start 
+    end
+
+    if goal:IsA("BasePart") then
+        self.goal = goal.CFrame
+    elseif goal:IsA("CFrame") then 
+        self.goal = goal.CFrame
+    end 
+end 
 return CameraSegway
