43bed5d40bd2ba316fd03098ac5025a2e2f8d289
Kecreeper <Kecreeper@gmail.com>
2024-07-17 03:07:18 -0400

    siffle

---
+++ init.lua
@@ -0,0 +1,22 @@
+local init = {}
+init.__index = init 
+
+local function siffleMainFolder(main: folder)
+    local primary = main.Primary
+    local secondary = main.Secondary
+
+    local pairings = {}
+
+    for _,v in pairs(primary:GetChildren()) do
+        table.insert(pairings, {v, secondary[v.Name]})
+    end
+
+    return pairings
+end
+
+function init.new(folder: folder)
+    local pairings = siffleMainFolder(folder)
+    self.pairings = pairings
+end 
+
+return init 
