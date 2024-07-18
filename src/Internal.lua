bf31c8ed179b1b2ca04a05f6d087fa5e80aca8bc
Kecreeper <Kecreeper@gmail.com>
2024-07-18 01:27:53 -0400

    fixes

--- src/Internal.lua
+++ src/Internal.lua
@@ -15,17 +15,11 @@ local function getCFrame(toGet: BasePart | CFrameValue | CFrame)
     end
 end
 
-local function changeCompleted(self)
-    self.tween.Completed:Connect(function()
-        self.CompletedBind:Fire()
-    end)
-end
-
 function Internal.new(start: BasePart | CFrame | CFrameValue, goal: BasePart | CFrame | CFrameValue, tweenInfo: TweenInfo)
     local self = setmetatable({}, Internal)
 
     self.start = getCFrame(start)
-    self.goal = getCFrame(goal)
+    self.goal = {CFrame = getCFrame(goal)}
     self.tweenInfo = tweenInfo
     self.CompletedBind = Instance.new("BindableEvent")
     self.Completed = self.CompletedBind.Event
@@ -38,19 +32,24 @@ function Internal:Play(cam: Camera)
         cam.CameraType = Enum.CameraType.Scriptable
         cam.CFrame = self.start
         self.tween:Play()
-        changeCompleted(self.tween)
     else
         cam.CameraType = Enum.CameraType.Scriptable
         cam.CFrame = self.start
         local tween = TWS:Create(cam, self.tweenInfo, self.goal)
         table.insert(self.tween, tween)
         tween:Play()
-        changeCompleted(tween)
+        tween.Completed:Connect(function()
+            self.CompletedBind:Fire()
+        end)
     end
 end
 
 function Internal:Cancel()
     self.tween:Cancel()
+end
+
+function Internal:Pause()
+    self.tween:Pause()
 end
 
 return Internal
